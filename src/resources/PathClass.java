e31aae59b6c7b6141f000f6612be8b587ef78ec0
kdogan <kamuran1905@yahoo.de>
2016-10-23 16:04:49 +0200

    #Bugfix fix absolute paths

--- src/resources/PathClass.java
+++ src/resources/PathClass.java
@@ -3,15 +3,15 @@ package resources;
 public class PathClass {
 
 	//FILEs
-	public static final String APPLICATION_CSS = "com/rechpro/ui/application.css";
+	public static final String APPLICATION_CSS = "/com/rechpro/ui/application.css";
 	public static final String RECHPRO_JFXML_FILE = "/resources/RechProjFXML.fxml";
 	
 	//BUTTONs
-	public static final String ADD_NEW_CUSTOMER_BTN = "com/rechpro/img/addNewCustomerBtn.png";
+	public static final String ADD_NEW_CUSTOMER_BTN = "/com/rechpro/img/addNewCustomerBtn.png";
 	
 	//ICONs
-	public static final String CUSTOMER_ICON_PATH = "com/rechpro/img/customerIcon.png";
-	public static final String RECHNUNG_ICON_PATH = "com/rechpro/img/rechnung.png";
-	public static final String SETTING_ICON_PATH = "com/rechpro/img/setting.png";
-	public static final String ARTICLE_ICON_PATH = "com/rechpro/img/ware.png";
+	public static final String CUSTOMER_ICON_PATH = "/com/rechpro/img/customerIcon.png";
+	public static final String RECHNUNG_ICON_PATH = "/com/rechpro/img/rechnung.png";
+	public static final String SETTING_ICON_PATH = "/com/rechpro/img/setting.png";
+	public static final String ARTICLE_ICON_PATH = "/com/rechpro/img/ware.png";
 }
