36cfbad13aa1b44702f2e21b6f7e4ef98e4d861d
kdogan <kamuran1905@yahoo.de>
2016-11-11 22:38:46 +0100

    #Refactoring Code clean up and Add helper class TableGenerator

--- src/com/rechpro/ui/RechnungArea.java
+++ src/com/rechpro/ui/RechnungArea.java
@@ -3,28 +3,23 @@ package com.rechpro.ui;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import com.rechpro.entity.Article;
 import com.rechpro.entity.Customer;
 import com.rechpro.persistence.DBService;
 import com.rechpro.transformer.CustomerTransformer;
-import com.rechpro.viewmodel.ArticleViewModel;
 import com.rechpro.viewmodel.ArticleViewModelInRechnung;
 import com.rechpro.viewmodel.CustomerViewModel;
-import com.rechpro.worker.ArticleController;
+import com.rechpro.worker.TableGenerator;
 
-import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -45,9 +40,12 @@ public class RechnungArea {
 	private DBService<Customer> dbService;
 	private CustomerViewModel customerViewModel;
 	private static Stage articleSelectStage;
+	private TableGenerator tableGenerator;
+	private TableView<ArticleViewModelInRechnung> articleTable;
 	
 	public final static ObservableList<ArticleViewModelInRechnung> articles = FXCollections.observableArrayList();
 	public RechnungArea() {
+		tableGenerator = new TableGenerator();
 		transformer = new CustomerTransformer();
         dbService = new DBService<Customer>("Customer");
         ArrayList<Customer> customers = (ArrayList<Customer>)dbService.getEntities();
@@ -59,7 +57,7 @@ public class RechnungArea {
 
 		GridPane grid = new GridPane();
 		BorderPane mainWinBorderPane = new BorderPane();
-		mainWinBorderPane.setPrefSize(700,1000);
+		mainWinBorderPane.setPrefSize(700, 1000);
 
 		VBox mainWindow = new VBox(20);
 		// ------------Header begin---------------------------//
@@ -75,98 +73,37 @@ public class RechnungArea {
 		center.setPrefSize(700, 400);
 		BorderPane centerTop = new BorderPane();
 		centerTop.setPrefHeight(100);
-		VBox miniAdress = new VBox(3);
-		Text sellerAddress = new Text("Körnerstr. 24 78777 Karlsruhe");
-		sellerAddress.setFont(Font.font("Verdana", 8));
-		Line line = new Line(90, 40, 350, 40);
-	    line.setStroke(Color.BLACK);
 
-		miniAdress.getChildren().addAll(sellerAddress, line);
-		centerTop.setTop(miniAdress);
-		// ------------Adress of seller end-------------------//
+		//////// (Right) Seller and Customer Address | (Left) Date, Customer and Rechnung Nr.  ////////
+		VBox sellerAddressMini = getSellerMiniAddress();
+		VBox leftColumn = getLeftColumn();
+		VBox rightColumn = getRightColumn();
+		VBox rechnungTextRow = getRechnungTextRow();
+		
+		centerTop.setTop(sellerAddressMini);
+		centerTop.setLeft(leftColumn);
+		centerTop.setRight(rightColumn);
+		centerTop.setBottom(rechnungTextRow);
+		
+		////////(Right) Seller and Customer Address | (Left) Date, Customer and Rechnung Nr.  end ////////
 
-		// ------- third row address of CustomerViewModel (left) date and id of bill (right) -----//
-		VBox customAndAddress = new VBox(5);
-		String customName = customerViewModel.getFirstName().get() + customerViewModel.getLastName().get();
-		Text custumName = new Text(customName);
-		//TODO: hier muss die Adresse nachdem Hausnummer in zweite Zeile geschrieben werden
-		String street = customerViewModel.getStreet().get();
-		String number = customerViewModel.getNo().get();
-		String postCode = customerViewModel.getPostCode().get();
-		String city = customerViewModel.getCity().get();
-
-		Text custumAdress = new Text(street+" " + number + "\n" + postCode+" "+city);
-		customAndAddress.getChildren().addAll(custumName, custumAdress);
-		centerTop.setLeft(customAndAddress);
-
-		VBox date = new VBox(5);
-		Text dateText = new Text("Datum: 12.12.2014");
-
-		Text customNr = new Text("KundenNr.: " + customerViewModel.getCustomerId().get());
-		Text billId = new Text("RechungsNr.: 1234567887");
-		date.getChildren().addAll(dateText, customNr, billId);
-
-		centerTop.setRight(date);
-		//customAndDate.getChildren().addAll(customAndAddress, date);
-
-		// ------- third row address of CustomerViewModel (left) date and id of bill (right) end-----//
-
-		// ------ text Rechung --------------------------------//
-		VBox billHBox = new VBox();
-		Label billText = new Label("RECHNUNG");
-		billText.setFont(new Font("Arial", 30));
-		Line line4 = new Line(90, 40, 800, 40);
-	    line4.setStroke(Color.BLACK);
-		billHBox.getChildren().addAll(billText,line4);
-		centerTop.setBottom(billHBox);
-		// ------ text Rechung end----------------------------//
-
-		// -------- list of items -----------------------------//
-		TableView<ArticleViewModelInRechnung> articleTable = new TableView<ArticleViewModelInRechnung>();
+		///////////////// list of selected articles //////////////////////////////
+		articleTable = tableGenerator.getSelectedArticleTable();
 		BorderPane centerButtom = new BorderPane();
-		
-		TableColumn<ArticleViewModelInRechnung,String> articleNumber = new TableColumn<ArticleViewModelInRechnung,String>("Artikel Nr.");
-		articleNumber.setCellValueFactory(new PropertyValueFactory<ArticleViewModelInRechnung,String>("articleNumber"));
-		articleNumber.prefWidthProperty().bind(articleTable.widthProperty().multiply(0.20));
-		
-		TableColumn<ArticleViewModelInRechnung,String> name = new TableColumn<ArticleViewModelInRechnung,String>("Artikel");
-		name.setCellValueFactory(new PropertyValueFactory<ArticleViewModelInRechnung,String>("name"));
-		name.prefWidthProperty().bind(articleTable.widthProperty().multiply(0.40));
-		//TODO: get number of article
-        TableColumn numberOfItem = new TableColumn<ArticleViewModelInRechnung,String>("Anzahl");
-        numberOfItem.setCellValueFactory(new PropertyValueFactory<ArticleViewModelInRechnung,String>("number"));
-        numberOfItem.prefWidthProperty().bind(articleTable.widthProperty().multiply(0.10));
-        
-        TableColumn<ArticleViewModelInRechnung,String> onePrise = new TableColumn<ArticleViewModelInRechnung,String>("Einzel Preis");
-        onePrise.setCellValueFactory(new PropertyValueFactory<ArticleViewModelInRechnung,String>("prise"));
-        onePrise.prefWidthProperty().bind(articleTable.widthProperty().multiply(0.15));
-       
-        TableColumn<ArticleViewModelInRechnung,String> entirePrise = new TableColumn<ArticleViewModelInRechnung,String>("Gesamt Preis");
-        entirePrise.setCellValueFactory(new PropertyValueFactory<ArticleViewModelInRechnung,String>("prise"));
-        entirePrise.prefWidthProperty().bind(articleTable.widthProperty().multiply(0.15));
-        
         articleTable.setItems(articles);
-        articleTable.getColumns().addAll(articleNumber, name, numberOfItem, onePrise, entirePrise);
-
 		centerButtom.setCenter(articleTable);
 		
 		final Button addButton = new Button("Ware Hinzufügen");
         addButton.setOnAction(e -> addCustomerSelectionArea());
 
-	//	midlePane.getChildren().addAll(miniAdress, customAndDate, warenList);
 		center.setTop(centerTop);
 		center.setCenter(centerButtom);
 		center.setBottom(addButton);
 		mainWinBorderPane.setCenter(center);
 		
-		articleTable.setOnMousePressed(e -> {
-			if (e.isPrimaryButtonDown() && e.getClickCount() == 1) {
-				ArticleViewModelInRechnung selectedArticle = articleTable.getSelectionModel().getSelectedItem();
-				updateArticleInTableView(selectedArticle);
-			}
-		});
+		articleTable.setOnMousePressed(e -> addSelectedArticleToTable(e));
 		
-		// -------- list of items end ------------------------//
+		///////////////// list of selected articles end /////////////////////////////
 
 		// --------- footer ----------------------------------//
 		VBox footer = new VBox(5);
@@ -189,12 +126,65 @@ public class RechnungArea {
 		return grid;
 	}
 	
-	private void updateArticleInTableView(ArticleViewModelInRechnung selectedArticle) {
-		for(ArticleViewModelInRechnung article: articles){
-			if(article.getArticleNumber() == selectedArticle.getArticleNumber()){
-				articles.remove(article);
-				articles.add(selectedArticle);
-			}
+	private VBox getLeftColumn() {
+		VBox customerAndAddress = new VBox(5);
+		String customName = customerViewModel.getFirstName().get() +" "+ customerViewModel.getLastName().get();
+		Text custumerName = new Text(customName);
+		//TODO: hier muss die Adresse nachdem Hausnummer in zweite Zeile geschrieben werden
+		String street = customerViewModel.getStreet().get();
+		String number = customerViewModel.getNo().get();
+		String postCode = customerViewModel.getPostCode().get();
+		String city = customerViewModel.getCity().get();
+
+		Text custumerAdress = new Text(street + " " + number + "\n" + postCode + " " + city);
+		customerAndAddress.getChildren().addAll(custumerName, custumerAdress);
+		
+		return customerAndAddress;
+	}
+
+	private VBox getRightColumn() {
+		VBox rightColumn = new VBox(5);
+		
+		Text dateText = new Text("Datum: 12.12.2014");
+		Text customerNr = new Text("KundenNr.: " + customerViewModel.getCustomerId().get());
+		Text rechnungNr = new Text("RechungsNr.: 1234567887");
+		
+		rightColumn.getChildren().addAll(dateText, customerNr, rechnungNr);
+		return rightColumn;
+	}
+
+	private VBox getRechnungTextRow() {
+		VBox billHBox = new VBox();
+		Label billText = new Label("RECHNUNG");
+		billText.setFont(new Font("Arial", 30));
+		Line line4 = new Line(90, 40, 800, 40);
+	    line4.setStroke(Color.BLACK);
+		billHBox.getChildren().addAll(billText,line4);
+		return billHBox;
+	}
+
+	private VBox getSellerMiniAddress() {
+		VBox miniAdress = new VBox(3);
+		// TODO : get address from from config class
+		Text sellerAddress = new Text("Körnerstr. 24 78777 Karlsruhe");
+		sellerAddress.setFont(Font.font("Verdana", 8));
+		Line line = new Line(90, 40, 350, 40);
+	    line.setStroke(Color.BLACK);
+		miniAdress.getChildren().addAll(sellerAddress, line);
+		
+		return miniAdress;
+	}
+
+	public static void closeArticleSelectStage(){
+		if (articleSelectStage == null)
+			return;
+		articleSelectStage.close();
+	}
+	
+	private void addSelectedArticleToTable(MouseEvent e) {
+		if (e.isPrimaryButtonDown() && e.getClickCount() == 1) {
+			ArticleViewModelInRechnung selectedArticle = articleTable.getSelectionModel().getSelectedItem();
+			updateArticleInTableView(selectedArticle);
 		}
 	}
 
@@ -209,16 +199,20 @@ public class RechnungArea {
 			e.printStackTrace();
 		}
 	}
-	public static void closeArticleSelectStage(){
-		if (articleSelectStage == null)
-			return;
-		articleSelectStage.close();
-	}
 	
 	private ImageView createImageView(String imgPath, int width, int hight) {
 		ImageView ImgView = new ImageView(new Image(getClass().getResourceAsStream(imgPath)));
 		ImgView.setFitHeight(hight);
 		ImgView.setFitWidth(width);
 		return ImgView;
+	}
+	
+	private void updateArticleInTableView(ArticleViewModelInRechnung selectedArticle) {
+		for(ArticleViewModelInRechnung article: articles){
+			if(article.getArticleNumber() == selectedArticle.getArticleNumber()){
+				articles.remove(article);
+				articles.add(selectedArticle);
+			}
+		}
 	}
 }
